c65c11eb52588a92584f67be2e788db9293d8e26
Jean Augusto <jean.aug02@gmail.com>
2024-01-27 19:53:16 -0300

    Inicio do projeto Criando tela de autenticacao

---
+++ src/pages/Home.tsx
@@ -0,0 +1,38 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logo_lightImg from '../assets/images/logo_light.svg'
+import logoGoogle from '../assets/images/google-icon.svg'
+
+import '../styles/auth.scss'
+
+export function Home() {
+    return (
+<div id="page-auth">
+    <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+    </aside>
+    <main>
+        <div className="main-content">
+            <img src={logo_lightImg} alt='Letmeask'/>
+            <button>
+                <img src={logoGoogle} alt='Logo Google'/>
+                Crie sua sala com o Google
+            </button> 
+            <div className='separator'>
+                ou entre em uma sala! 
+            </div>
+            <form>
+                <input 
+                    type="text"
+                    placeholder="Digite o código da sala"
+                /> 
+                <button type="submit">
+                    Entrar na sala
+                </button>
+            </form>
+        </div>
+    </main>
+</div>
+    )
+}
